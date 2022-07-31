efdaa0e08c7b12191f9dc2709fcf0c0c9fdddcfc
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2022-07-31 17:00:18 +0200

    Tests

--- lelele.py
+++ lelele.py
@@ -8,13 +8,23 @@ try:
 except ImportError:
     SOLVE = False
 
-def _wrap_lin(var):
-    if isinstance(var, LinearCombination):
-        return var
-    if isinstance(var, Variable):
-        return LinearCombination(ctx=var.ctx, combine=[(0x1, var)])
+def _wrap_lin(ctx, val):
+    assert isinstance(ctx, LeLeLe)
 
-    return int(var)
+    # linear combination
+    if isinstance(val, LinearCombination):
+        return val
+
+    # variable
+    if isinstance(val, Variable):
+        return val.lin()
+
+    # constant
+    try:
+        c = int(val)
+        return LinearCombination(ctx=ctx, combine=[(c, ctx.one())])
+    except ValueError:
+        raise ValueError('failed to covert %r to linear combination' % val)
 
 def _zero_matrix(n, m):
     M = []
@@ -25,21 +35,41 @@ def _zero_matrix(n, m):
 class LeLeLe:
     def __init__(self):
         self.vars = []
+        self.vone = None
         self.constraints = []
 
-    def var(self, name=None):
+    def one(self):
+        '''
+        Returns a "variable" which should the constant value one.
+        '''
+        if self.vone is None:
+            self.vone = self.bit(name='1')
+        return self.vone
+
+    def bit(self, name=None):
+        return self.var(name, 'bit').short(norm=1)
+
+    def byte(self, name=None):
+        return self.var(name, 'byte').short(norm=0xff)
+
+    def word(self, width, name=None):
+        assert width > 0
+        return self.var(name, 'word').short(norm=(1 << width)-1)
+
+    def var(self, name=None, prefix='var'):
         var = Variable()
         var.ctx = self
         if name is not None:
             var.name = name
         else:
-            var.name = 'v%d' % len(self.vars)
+            var.name = '%s%d' % (prefix, len(self.vars))
         var.index = len(self.vars)
         self.vars.append(var)
         return var
 
-    def add_constrain(self, lin):
-        self.constraints.append(lin)
+    def add_constraint(self, lin, norm):
+        assert isinstance(lin, LinearCombination)
+        self.constraints.append((lin, int(norm)))
 
     def system(self):
         '''
@@ -48,18 +78,22 @@ class LeLeLe:
         This does not require fpylll.
         '''
 
+        # calculathe largest norm
+        max_norm = max([norm for _, norm in self.constraints], default=1)
+
         rows = len(self.vars)
         cols = len(self.constraints)
 
         M = _zero_matrix(rows, cols)
 
-        for (i, con) in enumerate(self.constraints):
-            for (scale, var) in con.combine:
-                M[var.index][i] = scale
+        for (i, (lin, norm)) in enumerate(self.constraints):
+            rescale = max_norm // norm # rescale factor
+            for (scale, var) in lin.combine:
+                M[var.index][i] = scale * rescale
 
         return M
 
-    def solve(self, norm=None):
+    def solve(self):
         '''
         Solves the system and assigns the solution to the variables
         '''
@@ -72,7 +106,7 @@ class LeLeLe:
         # transformation (used to derieve assignment of variables)
         U = IntegerMatrix.identity(len(M))
 
-        # reduced basis (used to derieve assignment of constrains)
+        # reduced basis (used to derieve assignment of constraints)
         R = IntegerMatrix.from_matrix(M)
 
         # run LLL and save the result (for debugging)
@@ -87,9 +121,10 @@ class LeLeLe:
             var.solutions = []
 
         # reset solutions for each constraint
-        for con in self.constraints:
+        for (con, _) in self.constraints:
             con.solutions = []
 
+        # assign values to variables/constraints
         for row, assign in zip(R, U):
             if all([r == 0 for r in row]):
                 # trivial solution
@@ -104,15 +139,18 @@ class LeLeLe:
                 var.solutions.append(val)
 
             # add solutions to constraints
-            for con, val in zip(self.constraints, row):
+            for (con, _), val in zip(self.constraints, row):
                 con.solutions.append(val)
+
+        if self.vone and int(self.vone) != 1:
+            raise ValueError('.one() constant not assigned 1 in solution (assigned 0x%x); check the norms assigned with .short' % int(self.vone))
 
         return (self.R, self.U, self.M)
 
     def __repr__(self):
         cons = []
-        for con in self.constraints:
-            cons.append('    [%s]' % con)
+        for (lin, norm) in self.constraints:
+            cons.append('    0x%x = |%s|' % (norm,lin))
         return 'LLLSystem(\n' + '\n'.join(cons) + '\n)'
 
 class LinearCombination:
@@ -131,7 +169,7 @@ class LinearCombination:
         return True
 
     def __repr__(self):
-        lin = ['%s * %r' % (hex(s), v) if s != 1 else '%r' % v for (s, v) in self.combine]
+        lin = ['%r * %s' % (v, hex(s)) if s != 1 else '%r' % v for (s, v) in self.combine]
         return ' + '.join(lin)
 
     def __mod__(self, other):
@@ -159,18 +197,16 @@ class LinearCombination:
 
     def __add__(self, other):
         if other == 0: return self # this is convenient
-        assert isinstance(other, LinearCombination) or isinstance(other, Variable), 'can only add linear combination to linear combination, not %s' % type(other)
-        assert self.ctx == other.ctx, 'linear combinations belong to different systems'
 
         # combine terms
         # i.e. [(s1, var)] + [(s2, var)] would become [(s1 + s2, var)]
+        other = _wrap_lin(self.ctx, other)
 
-        lin1 = self.combine
-        lin2 = _wrap_lin(other).combine
+        assert self.ctx == other.ctx, 'linear combinations belong to different systems'
 
-        le_vars = {v: s for (s, v) in lin1}
+        le_vars = {v: s for (s, v) in self.combine}
 
-        for (s, v) in lin2:
+        for (s, v) in other.combine:
             try:
                 le_vars[v] += s
             except KeyError:
@@ -188,7 +224,7 @@ class LinearCombination:
         try:
             n = int(other)
         except ValueError:
-            raise ValueError('Can only mul. linear combination by integer')
+            raise ValueError('Can only mul. linear combination by integer, not %r' % other)
 
         return LinearCombination(
             ctx=self.ctx,
@@ -198,11 +234,15 @@ class LinearCombination:
     def __rmul__(self, other):
         return self.__mul__(other)
 
-    def short(self):
+    def short(self, norm=1):
         '''
         Constrain the linear combination to have small norm.
+
+        The "norm" parameter should be understood
+        as the "max-value" of the variable/linear combination.
         '''
-        self.ctx.add_constrain(self)
+        assert norm > 0
+        self.ctx.add_constraint(self, norm)
         return self
 
     def __getitem__(self, n):
@@ -227,23 +267,26 @@ class Variable:
     def __init__(self):
         self.solutions = None # not solved
 
-    def __sub__(self, other):
-        return _wrap_lin(self) - _wrap_lin(other)
+    def lin(self):
+        return LinearCombination(ctx=self.ctx, combine=[(0x1, self)])
 
     def __neg__(self):
-        return - _wrap_lin(self)
+        return - self.lin()
+
+    def __sub__(self, other):
+        return self.lin() - _wrap_lin(self.ctx, other)
 
     def __rsub__(self, other):
-        return _wrap_lin(self) - _wrap_lin(other)
+        return self.lin() - _wrap_lin(self.ctx, other)
 
     def __add__(self, other):
-        return _wrap_lin(self) + _wrap_lin(other)
+        return self.lin() + _wrap_lin(other)
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __mul__(self, other):
-        return _wrap_lin(self) * _wrap_lin(other)
+        return self.lin() * int(other)
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -255,13 +298,13 @@ class Variable:
         return int(self)
 
     def __mod__(self, other):
-        return _wrap_lin(self) % other
+        return self.lin() % int(other)
 
-    def short(self):
+    def short(self,norm=1):
         '''
         Constrain the variable to have small norm.
         '''
-        _wrap_lin(self).short()
+        self.lin().short(norm)
         return self
 
     def __getitem__(self, n):
---
+++ tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 ./test_oooooo.py
+python3 ./test_h1.py
